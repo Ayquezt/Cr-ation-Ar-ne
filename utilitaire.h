133bccae4264b1a653f908cbe363e7c8a337a555
Ayquezt <36932237+Ayquezt@users.noreply.github.com>
2018-03-11 17:52:01 +0100

    Create utilitaire.h

---
+++ utilitaire.h
@@ -0,0 +1,8 @@
+///Pour faire une pause entre chaque tour
+void pause();
+
+///Fonction pour nettoyer la console entre chaque tour
+void clearconsole();
+
+///Affichage de l'état du jeux au tour t
+void affichage_jeu(int t);

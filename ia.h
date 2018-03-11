48f62bfb07c17db77d25300ee13925a78d0d7234
Ayquezt <36932237+Ayquezt@users.noreply.github.com>
2018-03-11 17:50:40 +0100

    Create ia.h

---
+++ ia.h
@@ -0,0 +1,5 @@
+///Recherche l'ia sur le tableau pour voit son action
+void recherche_ia_action();
+
+///Recherche l'ia pour voir les interactions qui doivent se passer
+void recherche_ia_interaction();

91dab5eca6e12b1c6cf5f00d8a358d386c02ebe0
Ayquezt <36932237+Ayquezt@users.noreply.github.com>
2018-03-11 17:49:46 +0100

    Create balle.h

---
+++ balle.h
@@ -0,0 +1,8 @@
+///Place la balle au centre de l'arène
+void balle();
+
+///Pour que l'ia prenne la balle
+void balle_ia();
+
+///Quand il y a transfert de balle d'une ia à une autre
+void transfert_balle();
